f2ea2f6ca9fc9c242a1e329c7a4e3e30fa0e58cd
LeidyRosmery <rosm3rita@hotmail.com>
2017-09-26 18:49:47 -0500

    precio

--- assets/js/components/grid.js
+++ assets/js/components/grid.js
@@ -2,20 +2,32 @@
   var array = localStorage.getItem('carrito');
   array = JSON.parse(array);
 
-  const precioEnvio , precioPago ,precioProduct ,subPrecio, precioImpuesto , precioTotal,total;
+var precioEnvio , precioPago ,precioProduct,precioItem ,subPrecio, precioImpuesto , precioTotal,total;
   precioImpuesto=16*precioTotal;
   precioPago=0;
   precioEnvio=0;
-  precioTotal=precioPago+precioEnvio+subPrecio;
-  total=precioTotal-precioImpuesto;
+$('#envio').text('SOLES '+precioEnvio +'.00');
+$('#pago').text('SOLES '+precioPago +'.00');
+//  precioTotal=precioPago+precioEnvio+subPrecio;
+  //total=precioTotal-precioImpuesto;
     function render(){
     array.forEach(function(e,indice){
-    $('.grid-car').append('<div class="row " style="padding:.5em 0 ;display: flex;align-items: center;"><div class="col-sm-3 text-center"><img src="'+e.imagen+'" style="width:35% ; padding-left:35px" alt=""></div> <div class="col-sm-3 text-center"><div class="row"><div class="col-sm-6"><span><strong>'+e.nombre+'</strong></span></div><div class="col-sm-6"><span>PRECIO UNIT. '+e.precio+'</span></div></div>  </div><div class="col-sm-3 text-center"><div class="row">  <div class="col-sm-6"><select class="" style="width:60%" name=""><option value="">1</option><option value="">2</option>  <option value="">3</option><option value="">4</option><option value="">5</option><option value="">6</option></select></div><div class="col-sm-6"><i class="eliminar fa fa-trash fa-lg" data-delete="'+indice+'" aria-hidden="true"></i></div></div></div><div class="col-sm-3 text-center"><span>precio</span></div></div>');
-      console.log(e);
+    $('.grid-car').append('<div class="row " style="padding:.5em 0 ;display: flex;align-items: center;"><div class="col-sm-3 text-center"><img src="'+e.imagen+'" style="width:35% ; padding-left:35px" alt=""></div> <div class="col-sm-3 text-center"><div class="row"><div class="col-sm-6"><span><strong>'+e.nombre+'</strong></span></div><div class="col-sm-6"><span>PRECIO UNIT. '+e.precio+'</span></div></div>  </div><div class="col-sm-3 text-center"><div class="row">  <div class="col-sm-6"><select class="" style="width:60%"  data-precio="'+e.precio+' data-id="'+indice+'" name="cantidad"><option value="1">1</option><option value="2">2</option>  <option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option></select></div><div class="col-sm-6"><i class="eliminar fa fa-trash fa-lg" data-delete="'+indice+'" aria-hidden="true"></i></div></div></div><div class="col-sm-3 text-center"><span ><strong class="precio" data-id="'+indice+'"> '+e.precio*1+'</strong>  SOLES</span></div></div>');
+
     });
   }
 
 render();
+
+    $("select[name=cantidad]").change(function() {
+
+      console.log($(this).data('id'));
+        $('.precio').each(function() {
+          //  if($(this).data('id') ==valor){
+              console.log('hola');
+
+        });
+    });
 $('.grid-car').on('click', '.eliminar', function() {
     array.splice($(this).data('delete'), 1);
     $('.grid-car').empty();
