4eee5afbf36d1dadfe1316676d20eeb76431db7c
LeidyRosmery <rosm3rita@hotmail.com>
2017-09-25 22:20:52 -0500

    modal

--- assets/js/components/main.js
+++ assets/js/components/main.js
@@ -1,9 +1,9 @@
 'use strict';
-
+let carShopping = [];
 let category = [];
 $(document).ready(function() {
     gaseosa.forEach(function(e) {
-        
+
         $('.cont-brands').append('<li class="item-brand highlight">' + e.name + '</li>');
     });
 
@@ -25,14 +25,35 @@ $(document).ready(function() {
                 category = crucigrama;
         }
         category.forEach(function(e) {
-            //console.log(e);
             $('.cont-brands').append('<li class="item-brand highlight">' + e.name + '</li>');
-            console.log(e.name);
-            e.producto.forEach(function(elemento){
-              $('row-grid').append('<div class="col-sm-4"><div class="text-center"><img class="product img-responsive" src="'+elemento.img+'" alt=""><i class="trigger-modal icon-product fa fa-eye" data-toggle="modal" data-target="#exampleModal" aria-hidden="true"></i><i class="icon-product fa fa-shopping-basket" aria-hidden="true"></i><div class="bdr-top detail-product"><h3 class="title-product"> INKA KOLA</h3><p class="stock">Stock disponible</p> <p class="title-product">SOLES 20.02</p><p class="quantity">2 LITROS</p></div></div></div>');
+            if (e.producto != undefined) {
+                e.producto.forEach(function(elemento) {
+                    $('.row-grid').append(' <div class="col-sm-4" style="padding:1.5em"><div class="text-center"><img class="product img-responsive" src="'+elemento.img+'" alt=""><i class="trigger-modal icon-product fa fa-eye" data-nombre="'+e.name+'" data-cantidad="'+elemento.cantidad+'" data-precio="'+elemento.precio+'" data-stock="'+elemento.stock+'" data-imagen="'+elemento.img+'" data-toggle="modal" data-target="#exampleModal" aria-hidden="true"></i><i class="icon-product fa fa-shopping-basket" aria-hidden="true"></i> <div class="bdr-top detail-product"><h3 class="title-product">'+e.name+'</h3><p class="stock">'+elemento.stock+'</p><p class="title-product">SOLES '+elemento.precio+'</p><p class="quantity">'+elemento.cantidad+'</p></div></div></div>');
+                });
+            }
+        });
 
-            //  console.log(elemento.img);
-            });
-          });
     });
+
+    $('.row-grid').on('click', ".add-car", function() {
+        carShopping.push($(this).data('compra'));
+        console.log(carShopping);
+    });
+
+    $('.row-grid').on('click', ".trigger-modal", function() {
+
+        $('#img-modal').attr('src', $(this).data('imagen'));
+        $('#cantidad-modal').text($(this).data('cantidad'));
+        $('#precio-modal').text($(this).data('precio') + '.00 SOLES');
+        $('#stock-modal').text($(this).data('stock'));
+        $('#nombre-modal').text($(this).data('nombre'));
+    });
+    $('#icon-shopping').click(function() {
+        $('#all-items').text(carShopping.length + ' PRODUCTOS ');
+        carShopping.forEach(function(elemento) {
+            $('.cont-purchase').append('<div class="col-sm-12 item-purchase pad-x2"><div class="row pad-x3 "><div class="col-sm-4"><img class="product img-responsive" style="height:9vh" src="' + elemento.img + '" alt=""></div><div class="col-sm-8 text-center"><p class="text-center text-purchase mrg-0"><strong>GIVENCHY</strong></p><p class="text-center text-purchase mrg-0">M (INT)</p><p class="text-center text-purchase mrg-0">SOLES ' + elemento.precio + '</p></div></div></div>');
+        });
+
+    });
+
 });
--- assets/js/utilities/data.js
+++ assets/js/utilities/data.js
@@ -3,107 +3,107 @@ const cocaCola = [{
         img: 'https://upload.wikimedia.org/wikipedia/commons/b/be/Bouteille_de_Coca-Cola_d%27un_litre_cinq_001.jpg',
         stock: '20 unid.',
         precio: '13',
-        cantidad: ''
+        cantidad: '3.5L'
     },
     {
         id: '2',
         img: 'http://www.stashsafewarehouse.com/assets/images/products/cansafes/images/diet_coke_2l_cs_1.gif',
         stock: '60 unid.',
         precio: '3',
-        cantidad: ''
+        cantidad: '2L'
     },
     {
         id: '3',
         img: 'https://sc02.alicdn.com/kf/HTB171TvKFXXXXbbXVXXq6xXFXXXv/BOTTLE-2L.jpg',
         stock: '120 unid.',
         precio: '2',
-        cantidad: ''
+        cantidad: '3L'
     },
     {
         id: '4',
         img: 'https://upload.wikimedia.org/wikipedia/commons/3/3d/Coca-Cola_lata.jpg',
         stock: '350 unid.',
         precio: '5',
-        cantidad: ''
+        cantidad: '1L'
     },
     {
         id: '5',
         img: 'https://cloud10.todocoleccion.online/coleccionismo-coca-cola/tc/2010/01/23/17101631.jpg',
         stock: '280 unid.',
         precio: '12',
-        cantidad: ''
+        cantidad: '500mL'
     },
 ];
 const pepsi = [{
         id: '1',
         img: 'http://donkeys.com.ar/wp-content/uploads/2016/11/pepsi-lata.jpg',
-        stock: '20',
+        stock: '20 unid.',
         precio: '2',
-        cantidad: ''
+        cantidad: '500 mL'
     },
     {
         id: '2',
         img: 'https://sc01.alicdn.com/kf/UTB8nVx.byDEXKJk43Oq763z3XXaj/Pepsi-2-25L.png',
-        stock: '154',
+        stock: '154 unid.',
         precio: '15',
-        cantidad: ''
+        cantidad: '1L'
     },
     {
         id: '3',
         img: 'http://l7.alamy.com/zooms/c5f4376411f645ad8947db2326a73149/pepso-zero-diet-coke-can-eu-k15b06.jpg',
-        stock: '157',
+        stock: '157 unid.',
         precio: '13',
-        cantidad: ''
+        cantidad: '3L'
     },
     {
         id: '4',
         img: 'http://www.tesaliacbc.com/images/presentaciones_miniaturas/pepsi/image2.png',
-        stock: '654',
+        stock: '654 unid.',
         precio: '5',
-        cantidad: ''
+        cantidad: '1.5L'
     },
     {
         id: '5',
         img: 'http://img.bistekonline.com.br/Imagens/produtos/37/1353837/1353837_Detalhes.jpg',
-        stock: '87',
+        stock: '87 unid.',
         precio: '1',
-        cantidad: ''
+        cantidad: '3L'
     },
 ];
 const fanta = [{
         id: '1',
         img: 'http://2.bp.blogspot.com/-p6tNNh_Tfao/UK5UyeMxFMI/AAAAAAAAADE/80UA21y-8kc/s640/fanta_old_bottles.jpg',
-        stock: '98',
+        stock: '98 unid.',
         precio: '4.5',
-        cantidad: ''
+        cantidad: '1/2L'
     },
     {
         id: '2',
         img: 'http://www.grupohuarcaya.com/administrador/productos_menus2/images/20160107_1950_Qt7LBoZcVm.jpg',
-        stock: '54',
+        stock: '54 unid.',
         precio: '6',
-        cantidad: ''
+        cantidad: '1L'
     },
     {
         id: '3',
         img: 'https://www.rn-productoslatinos.nl/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/0/4/0403esp_fanta_naranja.jpg',
-        stock: '78',
+        stock: '78 unid.',
         precio: '5',
-        cantidad: ''
+        cantidad: '2L'
     },
     {
         id: '4',
         img: 'https://enemigapublica.files.wordpress.com/2009/06/fanta-blow-up1.jpg?w=645',
-        stock: '54',
+        stock: '54 unid.',
         precio: '2.5',
-        cantidad: ''
+        cantidad: '2.5L'
     },
     {
         id: '5',
         img: 'http://www.laconstancia.com/system/photos/397/medium/fanta.png?1395328395',
-        stock: '82',
+        stock: '82 unid.',
         precio: '1',
-        cantidad: ''
+        cantidad: '3L'
     },
 ];
 const gaseosa = [{
@@ -142,6 +142,42 @@ const gaseosa = [{
         name: 'KOLA REAL'
     },
 ];
-const chocolate = ["M&M", 'BELCOLADE', 'MONTBLANC', 'ALPEZZI', 'COVERLUX', 'KINDER', 'KIT KAT', 'SNIKERS'];
+const chocolate = [{
+        id: '1',
+        name: 'KINDER',
+      //  producto: kinder
+    },
+    {
+        id: '2',
+        name: 'SNICKERS',
+        //producto: snikers
+    },
+    {
+        id: '3',
+        name: 'KIT KAT',
+        //producto: kitkat
+    },
+    {
+        id: '4',
+        name: 'COVERLUX'
+    },
+    {
+        id: '5',
+        name: 'ALPEZZI'
+    },
+    {
+        id: '6',
+        name: 'MONTBLANC'
+    },
+    {
+        id: '7',
+        name: 'BELCOLADE'
+    },
+    {
+        id: '8',
+        name: '"M&M"'
+    },
+];
+
 const galleta = ["SODA FIELD", 'SAN JORGE', 'VAINILLA', 'CASINO', 'OREO', 'MOROCHAS', 'MARGARITA', 'CHOCO CHIPS'];
 const crucigrama = ["CAJA NEGRA", 'PEISA', 'ESTRUENDOMUDO', 'LA CASA DEL LIBRO VIEJO', 'MACRO', 'PALESTRA', 'FONDO EDITORIAL'];
