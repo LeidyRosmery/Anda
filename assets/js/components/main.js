44523bd270373de78a80044ea1ed4659625b95a2
LeidyRosmery <rosm3rita@hotmail.com>
2017-09-26 15:28:43 -0500

    implementando checkout

--- assets/js/components/main.js
+++ assets/js/components/main.js
@@ -6,7 +6,6 @@ $(document).ready(function() {
 
         $('.cont-brands').append('<li class="item-brand highlight">' + e.name + '</li>');
     });
-
     $('.category').click(function() {
         $('.cont-brands').empty();
         $('.row-grid').empty();
@@ -28,20 +27,28 @@ $(document).ready(function() {
             $('.cont-brands').append('<li class="item-brand highlight">' + e.name + '</li>');
             if (e.producto != undefined) {
                 e.producto.forEach(function(elemento) {
-                    $('.row-grid').append(' <div class="col-sm-4" style="padding:1.5em"><div class="text-center"><img class="product img-responsive" src="'+elemento.img+'" alt=""><i class="trigger-modal icon-product fa fa-eye" data-nombre="'+e.name+'" data-cantidad="'+elemento.cantidad+'" data-precio="'+elemento.precio+'" data-stock="'+elemento.stock+'" data-imagen="'+elemento.img+'" data-toggle="modal" data-target="#exampleModal" aria-hidden="true"></i><i class="icon-product fa fa-shopping-basket add-car"   aria-hidden="true"></i> <div class="bdr-top detail-product"><h3 class="title-product">'+e.name+'</h3><p class="stock">'+elemento.stock+'</p><p class="title-product">SOLES '+elemento.precio+'</p><p class="quantity">'+elemento.cantidad+'</p></div></div></div>');
-$('.add-car').data("bar",elemento);
+                    $('.row-grid').append(' <div class="col-sm-4" style="padding:1.5em"><div class="text-center"><img class="product img-responsive" src="'+elemento.img+'" alt=""><i class="trigger-modal icon-product fa fa-eye" data-nombre="'+e.name+'" data-cantidad="'+elemento.cantidad+'" data-precio="'+elemento.precio+'" data-stock="'+elemento.stock+'" data-imagen="'+elemento.img+'" data-toggle="modal" data-target="#exampleModal" aria-hidden="true"></i><i class="icon-product fa fa-shopping-basket add-car" aria-hidden="true"></i> <div class="bdr-top detail-product"><h3 class="title-product">'+e.name+'</h3><p class="stock">'+elemento.stock+'</p><p class="title-product">SOLES '+elemento.precio+'</p><p class="quantity">'+elemento.cantidad+'</p></div></div></div>');
+              $('.trigger-modal').data('elemento',elemento);
 
                 });
             }
         });
-
     });
-
     $('.row-grid').on('click', ".add-car", function() {
-        carShopping.push($(this).data('bar'));
-        console.log(carShopping);
-    });
+      const obj ={
+        nombre:$(this).prev().data('nombre'),
+        precio:$(this).prev().data('precio'),
+        cantidad:$(this).prev().data('cantidad'),
+        imagen:$(this).prev().data('imagen')
+      };
+        carShopping.push(obj);
+        localStorage.setItem("contador-bag", carShopping.length);
+    //   console.log(localStorage.getItem("contador-bag"));
+        $('.count-bag').text(carShopping.length);
 
+        $('.count-bag').css('display','inline-block');
+
+    });
     $('.row-grid').on('click', ".trigger-modal", function() {
 
         $('#img-modal').attr('src', $(this).data('imagen'));
@@ -53,9 +60,11 @@ $('.add-car').data("bar",elemento);
     $('#icon-shopping').click(function() {
         $('#all-items').text(carShopping.length + ' PRODUCTOS ');
         carShopping.forEach(function(elemento) {
-            $('.cont-purchase').append('<div class="col-sm-12 item-purchase pad-x2"><div class="row pad-x3 "><div class="col-sm-4"><img class="product img-responsive" style="height:9vh" src="' + elemento.img + '" alt=""></div><div class="col-sm-8 text-center"><p class="text-center text-purchase mrg-0"><strong>GIVENCHY</strong></p><p class="text-center text-purchase mrg-0">M (INT)</p><p class="text-center text-purchase mrg-0">SOLES ' + elemento.precio + '</p></div></div></div>');
+          console.log(elemento);
+            $('.cont-purchase').append('<div class="col-sm-12 item-purchase pad-x2"><div class="row pad-x3 "><div class="col-sm-4"><img class="product img-responsive" style="height:9vh" src="' + elemento.imagen + '" alt=""></div><div class="col-sm-8 text-center"><p class="text-center text-purchase mrg-0"><strong>'+elemento.nombre+'</strong></p><p class="text-center text-purchase mrg-0">(CANT) '+elemento.cantidad+'</p><p class="text-center text-purchase mrg-0">SOLES ' + elemento.precio + '</p></div></div></div>');
         });
 
     });
 
 });
+  $('.count-bagReg').text(localStorage.getItem("contador-bag"));
