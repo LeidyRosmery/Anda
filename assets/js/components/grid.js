acb8872723dd994981039f20a71f765cd4c6f688
LeidyRosmery <rosm3rita@hotmail.com>
2017-09-26 19:06:52 -0500

    compara indices

--- assets/js/components/grid.js
+++ assets/js/components/grid.js
@@ -12,7 +12,7 @@ $('#pago').text('SOLES '+precioPago +'.00');
   //total=precioTotal-precioImpuesto;
     function render(){
     array.forEach(function(e,indice){
-    $('.grid-car').append('<div class="row " style="padding:.5em 0 ;display: flex;align-items: center;"><div class="col-sm-3 text-center"><img src="'+e.imagen+'" style="width:35% ; padding-left:35px" alt=""></div> <div class="col-sm-3 text-center"><div class="row"><div class="col-sm-6"><span><strong>'+e.nombre+'</strong></span></div><div class="col-sm-6"><span>PRECIO UNIT. '+e.precio+'</span></div></div>  </div><div class="col-sm-3 text-center"><div class="row">  <div class="col-sm-6"><select class="" style="width:60%" data-precio="'+e.precio+'" name="cantidad"><option value="1">1</option><option value="2">2</option>  <option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option></select></div><div class="col-sm-6"><i class="eliminar fa fa-trash fa-lg" data-delete="'+indice+'" aria-hidden="true"></i></div></div></div><div class="col-sm-3 text-center"><span ><strong id="precio"> '+e.precio*1+'  </strong>  SOLES</span></div></div>');
+    $('.grid-car').append('<div class="row " style="padding:.5em 0 ;display: flex;align-items: center;"><div class="col-sm-3 text-center"><img src="'+e.imagen+'" style="width:35% ; padding-left:35px" alt=""></div> <div class="col-sm-3 text-center"><div class="row"><div class="col-sm-6"><span><strong>'+e.nombre+'</strong></span></div><div class="col-sm-6"><span>PRECIO UNIT. '+e.precio+'</span></div></div>  </div><div class="col-sm-3 text-center"><div class="row">  <div class="col-sm-6"><select class="" style="width:60%"  ata-precio="'+e.precio+'" name="cantidad"><option value="1">1</option><option value="2">2</option>  <option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option></select></div><div class="col-sm-6"><i class="eliminar fa fa-trash fa-lg" data-delete="'+indice+'" aria-hidden="true"></i></div></div></div><div class="col-sm-3 text-center"><span ><strong class="precioUnt" data-id="'+indice+'" id="precio"> '+e.precio*1+'  </strong>  SOLES</span></div></div>');
 
     });
   }
@@ -20,7 +20,11 @@ $('#pago').text('SOLES '+precioPago +'.00');
 render();
 
 $("select[name=cantidad]").change(function(){
+    //console.log($(this).data('ident'));
 $('#precio').empty();
+$('.precioUnt').each(function(){
+  console.log($(this).data('id'));
+});
 precioItem=($(this).val())*($(this).data('precio'));
 $('#precio').text(precioItem);
           //console.log(*);
