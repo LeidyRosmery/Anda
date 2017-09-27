646cb85ac404f5d1ab4655be7eb0d91bc185eb57
LeidyRosmery <rosm3rita@hotmail.com>
2017-09-26 19:38:40 -0500

    calcular montotoral

--- assets/js/components/grid.js
+++ assets/js/components/grid.js
@@ -5,11 +5,11 @@
 var precioEnvio , precioPago ,precioProduct,precioItem ,subPrecio, precioImpuesto , precioTotal,total;
   precioImpuesto=16*precioTotal;
   precioPago=0;
+  subPrecio=0;
   precioEnvio=0;
 $('#envio').text('SOLES '+precioEnvio +'.00');
 $('#pago').text('SOLES '+precioPago +'.00');
-//  precioTotal=precioPago+precioEnvio+subPrecio;
-  //total=precioTotal-precioImpuesto;
+
     function render(){
     array.forEach(function(e,indice){
     $('.grid-car').append('<div class="row " style="padding:.5em 0 ;display: flex;align-items: center;"><div class="col-sm-3 text-center"><img src="'+e.imagen+'" style="width:35% ; padding-left:35px" alt=""></div> <div class="col-sm-3 text-center"><div class="row"><div class="col-sm-6"><span><strong>'+e.nombre+'</strong></span></div><div class="col-sm-6"><span>PRECIO UNIT. '+e.precio+'</span></div></div>  </div><div class="col-sm-3 text-center"><div class="row">  <div class="col-sm-6"><select class="" style="width:60%" data-id="'+indice+'" data-precio="'+e.precio+'" name="cantidad"><option value="1">1</option><option value="2">2</option>  <option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option></select></div><div class="col-sm-6"><i class="eliminar fa fa-trash fa-lg" data-delete="'+indice+'" aria-hidden="true"></i></div></div></div><div class="col-sm-3 text-center"><span ><strong class="precioUnt" data-id="'+indice+'" id="precio"> '+e.precio*1+'  </strong>  SOLES</span></div></div>');
@@ -21,6 +21,8 @@ render();
 
 $("select[name=cantidad]").change(function() {
     var valor=$(this).data('id');
+    array[valor].precioTotal=50;
+  //localStorage.setItem("contador-bag", array);
         precioItem = ($(this).val()) * ($(this).data('precio'));
 
     $('.precioUnt').each(function() {
@@ -28,10 +30,14 @@ $("select[name=cantidad]").change(function() {
           $(this).empty();
           $(this).text(precioItem);
         };
-
     });
 
-
+  //  var array1 = localStorage.getItem('carrito');
+  //  array1 = JSON.parse(array1);
+    array.forEach(function(elemento){
+      subPrecio=subPrecio+elemento.precioTotal;
+    });
+    //$('#totalxProductos').text(subPrecio);
 });
 
 
--- assets/js/utilities/data.js
+++ assets/js/utilities/data.js
@@ -3,107 +3,122 @@ const cocaCola = [{
         img: 'https://upload.wikimedia.org/wikipedia/commons/b/be/Bouteille_de_Coca-Cola_d%27un_litre_cinq_001.jpg',
         stock: '20 unid.',
         precio: '13',
-        cantidad: '3.5L'
+        cantidad: '3.5L',
+        precioTotal:'0'
     },
     {
         id: '2',
         img: 'http://www.stashsafewarehouse.com/assets/images/products/cansafes/images/diet_coke_2l_cs_1.gif',
         stock: '60 unid.',
         precio: '3',
-        cantidad: '2L'
+        cantidad: '2L',
+        precioTotal:'0'
     },
     {
        id: '3',
         img: 'https://sc02.alicdn.com/kf/HTB171TvKFXXXXbbXVXXq6xXFXXXv/BOTTLE-2L.jpg',
         stock: '120 unid.',
         precio: '2',
-        cantidad: '3L'
+        cantidad: '3L',
+        precioTotal:'0'
     },
     {
         id: '4',
         img: 'https://upload.wikimedia.org/wikipedia/commons/3/3d/Coca-Cola_lata.jpg',
         stock: '350 unid.',
         precio: '5',
-        cantidad: '1L'
+        cantidad: '1L',
+        precioTotal:'0'
     },
     {
         id: '5',
         img: 'https://cloud10.todocoleccion.online/coleccionismo-coca-cola/tc/2010/01/23/17101631.jpg',
         stock: '280 unid.',
         precio: '12',
-        cantidad: '500mL'
+        cantidad: '500mL',
+        precioTotal:'0'
     },
 ];
 const pepsi = [{
-        id: '1',
+        id: '6',
         img: 'http://donkeys.com.ar/wp-content/uploads/2016/11/pepsi-lata.jpg',
         stock: '20 unid.',
         precio: '2',
-        cantidad: '500 mL'
+        cantidad: '500 mL',
+        precioTotal:'0'
     },
     {
-        id: '2',
+        id: '7',
         img: 'https://sc01.alicdn.com/kf/UTB8nVx.byDEXKJk43Oq763z3XXaj/Pepsi-2-25L.png',
         stock: '154 unid.',
         precio: '15',
-        cantidad: '1L'
+        cantidad: '1L',
+        precioTotal:'0'
     },
     {
-        id: '3',
+        id: '8',
         img: 'http://l7.alamy.com/zooms/c5f4376411f645ad8947db2326a73149/pepso-zero-diet-coke-can-eu-k15b06.jpg',
         stock: '157 unid.',
         precio: '13',
-        cantidad: '3L'
+        cantidad: '3L',
+        precioTotal:'0'
     },
     {
-        id: '4',
+        id: '9',
         img: 'http://www.tesaliacbc.com/images/presentaciones_miniaturas/pepsi/image2.png',
         stock: '654 unid.',
         precio: '5',
-        cantidad: '1.5L'
+        cantidad: '1.5L',
+        precioTotal:'0'
     },
     {
-        id: '5',
+        id: '10',
         img: 'http://img.bistekonline.com.br/Imagens/produtos/37/1353837/1353837_Detalhes.jpg',
         stock: '87 unid.',
         precio: '1',
-        cantidad: '3L'
+        cantidad: '3L',
+        precioTotal:'0'
     },
 ];
 const fanta = [{
-        id: '1',
+        id: '11',
         img: 'http://2.bp.blogspot.com/-p6tNNh_Tfao/UK5UyeMxFMI/AAAAAAAAADE/80UA21y-8kc/s640/fanta_old_bottles.jpg',
         stock: '98 unid.',
         precio: '4.5',
-        cantidad: '1/2L'
+        cantidad: '1/2L',
+        precioTotal:'0'
     },
     {
-        id: '2',
+        id: '12',
         img: 'http://www.grupohuarcaya.com/administrador/productos_menus2/images/20160107_1950_Qt7LBoZcVm.jpg',
         stock: '54 unid.',
         precio: '6',
-        cantidad: '1L'
+        cantidad: '1L',
+        precioTotal:'0'
     },
     {
-        id: '3',
+        id: '13',
         img: 'https://www.rn-productoslatinos.nl/media/catalog/product/cache/1/image/500x500/9df78eab33525d08d6e5fb8d27136e95/0/4/0403esp_fanta_naranja.jpg',
         stock: '78 unid.',
         precio: '5',
-        cantidad: '2L'
+        cantidad: '2L',
+        precioTotal:'0'
     },
     {
-        id: '4',
+        id: '14',
         img: 'https://enemigapublica.files.wordpress.com/2009/06/fanta-blow-up1.jpg?w=645',
         stock: '54 unid.',
         precio: '2.5',
-        cantidad: '2.5L'
+        cantidad: '2.5L',
+        precioTotal:'0'
     },
     {
-        id: '5',
+        id: '15',
         img: 'http://www.laconstancia.com/system/photos/397/medium/fanta.png?1395328395',
         stock: '82 unid.',
         precio: '1',
-        cantidad: '3L'
+        cantidad: '3L',
+        precioTotal:'0'
     },
 ];
 const gaseosa = [{
